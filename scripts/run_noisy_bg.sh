15a53732b7f8ffc11bf3c0b1a38eef1c0f741d72
Jiameng Fan <jmfan@bu.edu>
2021-01-25 22:39:52 -0500

    new scripts

--- scripts/run_noisy_bg.sh
+++ scripts/run_noisy_bg.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --task_name walk \
     --encoder_type pixel --work_dir ./log \
     --action_repeat 2 --num_eval_episodes 8 \
-    --pre_transform_image_size 100 --image_size 84 --noisy_bg True\
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
     --seed 0 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
 
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --task_name walk \
     --encoder_type pixel --work_dir ./log \
     --action_repeat 2 --num_eval_episodes 8 \
-    --pre_transform_image_size 100 --image_size 84 --noisy_bg True\
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
     --seed 1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
 
@@ -21,7 +21,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --task_name walk \
     --encoder_type pixel --work_dir ./log \
     --action_repeat 2 --num_eval_episodes 8 \
-    --pre_transform_image_size 100 --image_size 84 --noisy_bg True\
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
     --seed 2 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
 
@@ -30,7 +30,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --task_name walk \
     --encoder_type pixel --work_dir ./log \
     --action_repeat 2 --num_eval_episodes 8 \
-    --pre_transform_image_size 100 --image_size 84 --noisy_bg True\
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
     --seed 3 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
 
@@ -39,6 +39,6 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --task_name walk \
     --encoder_type pixel --work_dir ./log \
     --action_repeat 2 --num_eval_episodes 8 \
-    --pre_transform_image_size 100 --image_size 84 --noisy_bg True\
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
     --seed 4 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000
