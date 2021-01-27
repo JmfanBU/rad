0c6a5bee7f3013a2aa2f0d2900c5cb0d90b43ea5
Jiameng Fan <jmfan@bu.edu>
2021-01-27 00:53:31 -0500

    new scripts

---
+++ scripts/run_noisy_bg_cheetah.sh
@@ -0,0 +1,44 @@
+CUDA_VISIBLE_DEVICES=7 python train.py \
+    --domain_name cheetah \
+    --task_name run \
+    --encoder_type pixel --work_dir ./log \
+    --action_repeat 4 --num_eval_episodes 8 \
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
+    --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
+    --seed 0 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+    --domain_name cheetah \
+    --task_name run \
+    --encoder_type pixel --work_dir ./log \
+    --action_repeat 4 --num_eval_episodes 8 \
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
+    --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
+    --seed 1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+    --domain_name cheetah \
+    --task_name run \
+    --encoder_type pixel --work_dir ./log \
+    --action_repeat 4 --num_eval_episodes 8 \
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
+    --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
+    --seed 2 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+    --domain_name cheetah \
+    --task_name run \
+    --encoder_type pixel --work_dir ./log \
+    --action_repeat 4 --num_eval_episodes 8 \
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
+    --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
+    --seed 3 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+    --domain_name cheetah \
+    --task_name run \
+    --encoder_type pixel --work_dir ./log \
+    --action_repeat 4 --num_eval_episodes 8 \
+    --pre_transform_image_size 100 --image_size 84 --noisy_bg \
+    --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
+    --seed 4 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000
--- scripts/run_noisy_bg_walk.sh
+++ scripts/run_noisy_bg_walk.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --action_repeat 2 --num_eval_episodes 8 \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
-    --seed 0 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
+    --seed 0 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
 
 CUDA_VISIBLE_DEVICES=7 python train.py \
     --domain_name walker \
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --action_repeat 2 --num_eval_episodes 8 \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
-    --seed 1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
+    --seed 1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
 
 CUDA_VISIBLE_DEVICES=7 python train.py \
     --domain_name walker \
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --action_repeat 2 --num_eval_episodes 8 \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
-    --seed 2 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
+    --seed 2 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
 
 CUDA_VISIBLE_DEVICES=7 python train.py \
     --domain_name walker \
@@ -32,7 +32,7 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --action_repeat 2 --num_eval_episodes 8 \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
-    --seed 3 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 &&
+    --seed 3 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000 &&
 
 CUDA_VISIBLE_DEVICES=7 python train.py \
     --domain_name walker \
@@ -41,4 +41,4 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
     --action_repeat 2 --num_eval_episodes 8 \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent rad_sac --frame_stack 3 --data_augs crop-grayscale  \
-    --seed 4 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000
+    --seed 4 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 880000
